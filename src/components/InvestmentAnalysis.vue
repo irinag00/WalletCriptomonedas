34168e424d53fd8fbf0c9f376a35773125e138d5
irinag00 <irinagorlino@gmail.com>
2023-12-12 02:26:08 -0300

    agregué algunos loader

--- src/components/InvestmentAnalysis.vue
+++ src/components/InvestmentAnalysis.vue
@@ -1,11 +1,16 @@
 <script>
 import { useUserStore } from "../stores/user";
 import { useTransactionStore } from "../stores/transaction";
+import Loader from "../components/Loader.vue";
 
 export default {
+  components: {
+    Loader,
+  },
   data() {
     return {
       balances: {},
+      loading: false,
     };
   },
   mounted() {
@@ -18,12 +23,13 @@ export default {
 
       const balanceStore = useTransactionStore();
       this.balances = await balanceStore.calculateCryptoBalances(user);
+      this.loading = true;
     },
   },
 };
 </script>
 <template>
-  <div v-if="Object.keys(balances).length === 0">
+  <!-- <div v-if="Object.keys(balances).length === 0">
     <div className="flex flex-col items-center justify-center m-3 p-3">
       <i
         class="bi bi-cash-coin mb-1 text-gray-600"
@@ -36,46 +42,53 @@ export default {
         Empieza a comprar y vender criptos para cambiar tu vida...
       </h3>
     </div>
+  </div> -->
+  <div v-if="!loading" class="flex justify-center mt-40">
+    <Loader></Loader>
   </div>
-  <div v-else class="border-t border-gray-200 mt-10">
-    <h2 class="text-2xl font-bold mt-5 ml-6">Inversiones</h2>
-    <div
-      class="relative overflow-x-auto rounded-lg border border-gray-200 shadow-md m-5"
-    >
-      <table
-        class="w-full border-collapse bg-white text-left text-base text-gray-500 text-center"
+  <div v-if="loading">
+    <div class="border-t border-gray-200 mt-10">
+      <h2 class="text-2xl font-bold mt-5 ml-6">Inversiones</h2>
+      <div
+        class="relative overflow-x-auto rounded-lg border border-gray-200 shadow-md m-5"
       >
-        <thead class="bg-gray-50">
-          <tr>
-            <th scope="col" class="px-6 py-4 font-medium text-gray-900">
-              Criptomoneda
-            </th>
-            <th scope="col" class="px-6 py-4 font-medium text-gray-900">
-              Resultado
-            </th>
-          </tr>
-        </thead>
-        <tbody class="divide-y divide-gray-100 border-t border-gray-100">
-          <tr
-            class="hover:bg-gray-50"
-            div
-            v-for="(valor, clave) in balances"
-            :key="clave"
-          >
-            <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-              <h3>{{ clave.toUpperCase() }}</h3>
-            </td>
-            <td class="px-6 py-4" v-if="valor.profitLoss < 0">
-              <h3 class="font-semibold text-red-600">{{ valor.profitLoss }}</h3>
-            </td>
-            <td class="px-6 py-4" v-else-if="valor.profitLoss > 0">
-              <h3 class="font-semibold text-green-600">
-                {{ valor.profitLoss }}
-              </h3>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+        <table
+          class="w-full border-collapse bg-white text-left text-base text-gray-500 text-center"
+        >
+          <thead class="bg-gray-50">
+            <tr>
+              <th scope="col" class="px-6 py-4 font-medium text-gray-900">
+                Criptomoneda
+              </th>
+              <th scope="col" class="px-6 py-4 font-medium text-gray-900">
+                Resultado
+              </th>
+            </tr>
+          </thead>
+          <tbody class="divide-y divide-gray-100 border-t border-gray-100">
+            <tr
+              class="hover:bg-gray-50"
+              div
+              v-for="(valor, clave) in balances"
+              :key="clave"
+            >
+              <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                <h3>{{ clave.toUpperCase() }}</h3>
+              </td>
+              <td class="px-6 py-4" v-if="valor.profitLoss < 0">
+                <h3 class="font-semibold text-red-600">
+                  {{ valor.profitLoss }}
+                </h3>
+              </td>
+              <td class="px-6 py-4" v-else-if="valor.profitLoss > 0">
+                <h3 class="font-semibold text-green-600">
+                  {{ valor.profitLoss }}
+                </h3>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
--- src/views/InvestmentView.vue
+++ src/views/InvestmentView.vue
@@ -1,6 +1,7 @@
 <script>
 import Navbar from "../components/layout/Navbar.vue";
 import Hero from "../components/layout/Hero.vue";
+import Loader from "../components/Loader.vue";
 import { useUserStore } from "../stores/user";
 import { useTransactionStore } from "../stores/transaction";
 import InvestmentAnalysis from "../components/InvestmentAnalysis.vue";
@@ -9,6 +10,7 @@ export default {
     Navbar,
     Hero,
     InvestmentAnalysis,
+    Loader,
   },
   data() {
     return {
@@ -16,6 +18,7 @@ export default {
       user: "",
       balances: {},
       totalMoney: 0,
+      loading: false,
     };
   },
   async created() {
@@ -25,6 +28,7 @@ export default {
     const balanceStore = useTransactionStore();
     this.balances = await balanceStore.calculateCryptoBalances(this.user);
     this.balanceTotalMoney();
+    this.loading = true;
   },
   methods: {
     balanceTotalMoney() {
@@ -42,53 +46,78 @@ export default {
   <Navbar></Navbar>
   <div class="p-4 sm:ml-64">
     <Hero :sectionName="nameSection"></Hero>
-    <h2 class="text-2xl font-bold mt-5 ml-6">Estado Actual</h2>
-    <div
-      class="relative overflow-x-auto rounded-lg border border-gray-200 shadow-md m-5"
-    >
-      <table
-        class="w-full border-collapse bg-white text-left text-base text-gray-500 text-center"
-      >
-        <thead class="bg-gray-50">
-          <tr>
-            <th scope="col" class="px-6 py-4 font-medium text-gray-900">
-              Criptomoneda
-            </th>
-            <th scope="col" class="px-6 py-4 font-medium text-gray-900">
-              Cantidad
-            </th>
-            <th scope="col" class="px-6 py-4 font-medium text-gray-900">
-              Monto en ARS
-            </th>
-          </tr>
-        </thead>
-        <tbody class="divide-y divide-gray-100 border-t border-gray-100">
-          <tr
-            class="hover:bg-gray-50"
-            div
-            v-for="(valor, clave) in balances"
-            :key="clave"
-            v-show="valor.totalCryptoAmount > 0"
+    <div v-if="!loading" class="flex justify-center mt-40">
+      <Loader></Loader>
+    </div>
+    <div v-if="loading">
+      <div v-if="Object.keys(balances).length === 0">
+        <div className="flex flex-col items-center justify-center m-3 p-3">
+          <i
+            class="bi bi-cash-coin mb-1 text-gray-600"
+            style="font-size: 100px"
+          ></i>
+          <h2 class="text-2xl font-bold text-cyan-600 my-2">
+            ¡Aún no has realizado transacciones!
+          </h2>
+          <h3 class="text-base font-semibold text-gray-600 my-2">
+            Empieza a comprar y vender criptos para cambiar tu vida...
+          </h3>
+        </div>
+      </div>
+      <div v-else>
+        <h2 class="text-2xl font-bold mt-5 ml-6">Estado Actual</h2>
+        <div
+          class="relative overflow-x-auto rounded-lg border border-gray-200 shadow-md m-5"
+        >
+          <table
+            class="w-full border-collapse bg-white text-left text-base text-gray-500 text-center"
           >
-            <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-              <h3>{{ clave.toUpperCase() }}</h3>
-            </td>
-            <td class="px-6 py-4">
-              <h3>{{ valor.totalCryptoAmount }}</h3>
-            </td>
-            <td class="px-6 py-4">
-              <h3>{{ valor.totalMoney.toFixed(2) }} ARS</h3>
-            </td>
-          </tr>
-        </tbody>
-        <tfoot class="divide-y divide-gray-100 border-t border-gray-100">
-          <tr class="font-semibold text-gray-900 dark:text-white bg-cyan-500">
-            <th scope="row" class="px-6 py-3 text-base">Total</th>
-            <td class="px-6 py-3"></td>
-            <td class="px-6 py-3">{{ totalMoney.toFixed(2) }} ARS</td>
-          </tr>
-        </tfoot>
-      </table>
+            <thead class="bg-gray-50">
+              <tr>
+                <th scope="col" class="px-6 py-4 font-medium text-gray-900">
+                  Criptomoneda
+                </th>
+                <th scope="col" class="px-6 py-4 font-medium text-gray-900">
+                  Cantidad
+                </th>
+                <th scope="col" class="px-6 py-4 font-medium text-gray-900">
+                  Monto en ARS
+                </th>
+              </tr>
+            </thead>
+            <tbody class="divide-y divide-gray-100 border-t border-gray-100">
+              <tr
+                class="hover:bg-gray-50"
+                div
+                v-for="(valor, clave) in balances"
+                :key="clave"
+                v-show="valor.totalCryptoAmount > 0"
+              >
+                <td
+                  class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  <h3>{{ clave.toUpperCase() }}</h3>
+                </td>
+                <td class="px-6 py-4">
+                  <h3>{{ valor.totalCryptoAmount }}</h3>
+                </td>
+                <td class="px-6 py-4">
+                  <h3>{{ valor.totalMoney.toFixed(2) }} ARS</h3>
+                </td>
+              </tr>
+            </tbody>
+            <tfoot class="divide-y divide-gray-100 border-t border-gray-100">
+              <tr
+                class="font-semibold text-gray-900 dark:text-white bg-cyan-500"
+              >
+                <th scope="row" class="px-6 py-3 text-base">Total</th>
+                <td class="px-6 py-3"></td>
+                <td class="px-6 py-3">{{ totalMoney.toFixed(2) }} ARS</td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+      </div>
     </div>
     <InvestmentAnalysis></InvestmentAnalysis>
   </div>
